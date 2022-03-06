92ade3a12dca64b38de4e7ea799791c4f2ab1b71
Atereoyinn <98267555+Atereoyinn@users.noreply.github.com>
2022-03-06 11:20:54 -0800

    Update Calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 const Calculator = () => {
-
+    /* eslint no-eval: 0 */
     //Destrusturing props of useState
     const [results,setResult] = useState("");
     const clickHandler = (e) => {
